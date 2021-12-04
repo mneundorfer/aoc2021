7f558011319265b16d5ba0fa0f6ca164ba8bf07a
Maximilian Neundorfer <code@mneundorfer.de>
2021-12-04 18:56:35 +0100

    aoc d1 pt1

---
+++ d1/Program.cs
@@ -0,0 +1,12 @@
+﻿int? previous = null;
+int increased = 0;
+foreach (string line in File.ReadLines(@"input.txt"))
+{
+    int x = Int32.Parse(line);
+    if (previous != null && x > previous) {
+        increased++;
+    }
+    previous = x;
+}
+
+Console.WriteLine(increased);
