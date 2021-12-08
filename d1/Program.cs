f093d6230c69e998ab4bb811bba3c5a88abb4e55
Maximilian Neundorfer <code@mneundorfer.de>
2021-12-08 20:44:42 +0100

    d1

--- d1/Program.cs
+++ d1/Program.cs
@@ -1,12 +1,32 @@
-﻿int? previous = null;
+﻿IEnumerable<string> input = File.ReadLines(@"input.txt");
+
+int? previous = null;
 int increased = 0;
-foreach (string line in File.ReadLines(@"input.txt"))
+foreach (string line in input)
 {
     int x = Int32.Parse(line);
-    if (previous != null && x > previous) {
+    if (previous != null && x > previous)
+    {
         increased++;
     }
     previous = x;
 }
 
-Console.WriteLine(increased);
+Console.WriteLine($"pt1: {increased}");
+
+IEnumerable<int> inputsAsInts = input.Select(Int32.Parse);
+var previousThreeWindowItemSum = inputsAsInts.Take(3).Sum();
+int increasedWindows = 0;
+
+for (int i = 0; i < input.Count(); i++)
+{
+    var currentThreeItemWindowSum = inputsAsInts.Skip(i).Take(3).Sum();
+    if (currentThreeItemWindowSum > previousThreeWindowItemSum)
+    {
+        increasedWindows++;
+    }
+    previousThreeWindowItemSum = currentThreeItemWindowSum;
+
+}
+
+Console.WriteLine($"pt2: {increasedWindows}");
