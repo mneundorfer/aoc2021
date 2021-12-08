3b32fcc61ccb8195a44b896a74e9053b87c401be
Maximilian Neundorfer <code@mneundorfer.de>
2021-12-08 21:15:49 +0100

    d2 pt1

---
+++ d2/Program.cs
@@ -0,0 +1,24 @@
+﻿IEnumerable<string> input = File.ReadLines(@"input.txt");
+
+int position = 0;
+int depth = 0;
+
+foreach (string line in input) {
+    string[] vals = line.Split(' ');
+    string type = vals[0];
+    int amount = Int32.Parse(vals[1]);
+    if (type == "forward")
+    {
+        position += amount;
+    }
+    else if (type == "down")
+    {
+        depth += amount;
+    }
+    else if (type == "up")
+    {
+        depth -= amount;
+    }
+}
+
+Console.WriteLine($"pt1: {position * depth}");
